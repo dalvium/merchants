da9748e9ffaab2b73cacf41ec7d3ae26a56da363
Vladimir <dontuvlad87@gmail.com>
2021-03-15 13:12:54 +0200

    Authentification using Devise gem for Merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,8 +1,11 @@
 class Merchant < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   store_accessor :extra, :phone_number, :website
 
-  validates :name, presence: true
   validates :email, format: URI::MailTo::EMAIL_REGEXP
 
 
